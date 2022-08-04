c6301fd4ff1766b8b0fa0102336717e064b896b3
h110m <nichtwitzig228@gmail.com>
2022-08-04 20:46:14 +0200

    Added installation guide for no-gui systems and copy button for code blocks (#8)

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -2,11 +2,12 @@ module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: 'whatsapp-web.js',
+  title: "whatsapp-web.js",
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
-  description: 'A WhatsApp client library for NodeJS that connects through the WhatsApp Web browser app',
+  description:
+    "A WhatsApp client library for NodeJS that connects through the WhatsApp Web browser app",
 
   /**
    * Extra tags to be injected to the page HTML `<head>`
@@ -14,9 +15,12 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/config/#head
    */
   head: [
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+    ["meta", { name: "theme-color", content: "#3eaf7c" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" },
+    ],
   ],
 
   /**
@@ -25,55 +29,62 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
    */
   themeConfig: {
-    repo: 'pedroslopez/whatsapp-web.js',
-    docsRepo: 'wwebjs/wwebjs.dev',
-    docsBranch: 'main',
+    repo: "pedroslopez/whatsapp-web.js",
+    docsRepo: "wwebjs/wwebjs.dev",
+    docsBranch: "main",
     editLinks: true,
-    docsDir: 'src',
-    lastUpdated: 'Last Updated',
+    docsDir: "src",
+    lastUpdated: "Last Updated",
     sidebarDepth: 3,
     nav: [
       {
-        text: 'Guide',
-        link: '/guide/',
+        text: "Guide",
+        link: "/guide/",
       },
       {
-        text: 'Documentation',
-        link: 'https://docs.wwebjs.dev'
+        text: "Documentation",
+        link: "https://docs.wwebjs.dev",
       },
       {
-        text: 'npm',
-        link: 'https://npmjs.com/package/whatsapp-web.js'
+        text: "npm",
+        link: "https://npmjs.com/package/whatsapp-web.js",
       },
       {
-        text: 'Releases',
-        link: 'https://github.com/pedroslopez/whatsapp-web.js/releases'
-      }
+        text: "Releases",
+        link: "https://github.com/pedroslopez/whatsapp-web.js/releases",
+      },
     ],
     sidebar: {
-      '/guide/': [
+      "/guide/": [
         {
-          title: 'Guide',
+          title: "Guide",
           collapsable: false,
           children: [
-            '',
-            'authentication',
-            'handling-attachments',
-            'mentioning-contacts',
-          ]
-        }
-      ]
-    }
+            "",
+            "authentication",
+            "handling-attachments",
+            "mentioning-contacts",
+          ],
+        },
+      ],
+    },
   },
 
-  theme: 'yuu',
-
+  theme: "yuu",
 
   /**
    * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
    */
   plugins: [
-    '@vuepress/plugin-back-to-top',
-    '@vuepress/plugin-medium-zoom',
-  ]
-}
+    "@vuepress/plugin-back-to-top",
+    "@vuepress/plugin-medium-zoom",
+    [
+      "vuepress-plugin-code-copy",
+      {
+        align: "bottom",
+        color: "#42b983",
+        backgroundColor: "#42b983",
+      },
+    ],
+  ],
+};
