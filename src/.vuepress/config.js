ade979b61ffd8c3751a90aeed371b70c8d94d8b2
GhtKarma <marcniklas11@gmail.com>
2022-08-18 00:59:06 +0200

    update fix

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -59,33 +59,34 @@ module.exports = {
     ],
     sidebar: {
       "/guide/": [
-      {
-        title: "Guide",
-        children: [
-          "/guide/",
-          "/guide/getting-started",
-          "/guide/authentication",
-          "/guide/creating-your-app",
-          "/guide/how-to-use",
-        ],
-      },
-      {
-        title: "Interactions",
-        children: ["/interactions/"],
-      },
-      {
-        title: "Advanced",
-        children: ["/advanced/"],
-      },
-      {
-        title: "Popular Topics",
-        children: ["/popularTopics/"],
-      },
-      {
-        title: "Additional Information",
-        children: ["/additionalInformation/"],
-      },
-    ],
+        {
+          title: "Guide",
+          children: [
+            "/guide/",
+            "/guide/getting-started",
+            "/guide/authentication",
+            "/guide/creating-your-app",
+            "/guide/how-to-use",
+          ],
+        },
+        {
+          title: "Interactions",
+          children: ["/interactions/"],
+        },
+        {
+          title: "Advanced",
+          children: ["/advanced/"],
+        },
+        {
+          title: "Popular Topics",
+          children: ["/popularTopics/"],
+        },
+        {
+          title: "Additional Information",
+          children: ["/additionalInformation/"],
+        },
+      ],
+    },
   },
 
   theme: "yuu",
