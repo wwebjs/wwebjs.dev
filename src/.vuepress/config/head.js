8df14e06aa9db69af65a58c88a908a3a1d664dfd
qzdark <marcniklas11@gmail.com>
2024-06-16 17:27:26 +0200

    add: embed card

--- src/.vuepress/config/head.js
+++ src/.vuepress/config/head.js
@@ -5,4 +5,12 @@ export const head = [
     ['meta', { name: 'theme-color', content: '#25D366' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+
+    ['meta', { name: 'twitter:card', content: 'summary' }],
+    ['meta', { property: 'og:title', content: 'WWebJS Guide' }],
+    ['meta', { property: 'og:description', content: "whatsapp-web.js is an unofficial, open-source library it is designed to offer developers the freedom to create WhatsApp clients, chatbots, applications, and more using node.js." }],
+    ['meta', { property: 'og:type', content: 'website' }],
+    ['meta', { property: 'og:url', content: 'https://wwebjs.dev/' }],
+    ['meta', { property: 'og:locale', content: 'en_US' }],
+    ['meta', { property: 'og:image', content: '/images/banner_logo.png' }],
 ];
