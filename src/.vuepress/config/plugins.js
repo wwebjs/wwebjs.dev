9b8b5b6c7d86dd941642f5d035f3ecc0a0bc13fe
qzdark <marcniklas11@gmail.com>
2024-06-30 01:58:24 +0200

    fix indexName

--- src/.vuepress/config/plugins.js
+++ src/.vuepress/config/plugins.js
@@ -4,7 +4,7 @@ export const plugins = [
   docsearchPlugin({
     appId: '8AJ0E0DA1M',
     apiKey: 'fcc76d2a115c516c8d01512303f43a8b',
-    indexName: 'wwebjs_guide',
+    indexName: 'wwebjs',
     locales: {
       '/': {
         placeholder: 'Search',
